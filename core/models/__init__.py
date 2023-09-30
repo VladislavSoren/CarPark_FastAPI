3daad5a352c93e206bce7548b1f68f1c0bc4ca92
VladSoren <vlad1998ss@mail.ru>
2023-09-30 11:27:02 +0300

    changed isort config: all __init__.py skipped due to import Errors

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -8,8 +8,11 @@ __all__ = (
     "Auto",
 )
 
-from .auto import Auto
+# Base import must be first (to escape ImportError: circular import)
+# all __init__.py files skipped for "isort" validator
 from .base import Base
+
+from .auto import Auto
 from .db_helper import DatabaseHelper, db_helper
 from .park import Park
 from .park_owner import ParkOwner
